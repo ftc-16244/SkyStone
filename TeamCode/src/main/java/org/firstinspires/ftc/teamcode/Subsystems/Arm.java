5d4f5a1b9ea4ec3c6111062599e205409202d7aa
David Atkinon <david.atkinson@netzero.com>
2020-06-10 22:08:30 -0500

    misc changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Arm.java
@@ -5,36 +5,38 @@ import android.media.MediaPlayer;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
-public class Arm
-{
+public class Arm {
 
     // Define hardware objects
-    public DcMotor      armLeft      = null;
-    public DcMotor      armRight     = null;
+    public DcMotor armLeft = null;
+    public DcMotor armRight = null;
     // adding a comment to demo a git pull command
 
     private static final int ARM_STONE_LOAD = 50; // Left side reference
     private static final int ARM_STONE_CARRY = 200;// Left side reference
     private static final double ARM_SPEED = .5;
-    private static final double ARM_RESET_POWER = .1;
+    private static final double ARM_RESET_POWER = .5;
 
-    private ElapsedTime     runtime = new ElapsedTime();
+    private ElapsedTime runtime = new ElapsedTime();
 
     // Contructor for Arm
-    public Arm(){
+    public Arm() {
 
     }
 
-    public void init(HardwareMap hwMap){
+    public void init(HardwareMap hwMap) {
+
 
         armLeft = hwMap.get(DcMotor.class,"Arm");
         armRight =  hwMap.get(DcMotor.class,"Arm_2");
 
-        armLeft.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if needed
+
+        armLeft.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if needed
         armRight.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if needed
 
         armLeft.setPower(0);
@@ -49,7 +51,7 @@ public class Arm
         armRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
-    public void moveToPickupStone()  {
+    public void moveToPickupStone() {
         armLeft.setTargetPosition(ARM_STONE_LOAD);
         armRight.setTargetPosition(ARM_STONE_LOAD);
         armLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -71,12 +73,18 @@ public class Arm
 
     }
 
-    public void resetArmPosn(){
+    public void resetArmPosn() {
         runtime.reset();
+
+        armLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        armRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        armLeft.setPower(-.3);
+        armRight.setPower(-.3);
         while (runtime.seconds() < 3.0) {
-            armLeft.setPower(- ARM_RESET_POWER);
-            armRight.setPower(-ARM_RESET_POWER);
+            //telemetry.addData("Arm Resetting", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+
         }
+<<<<<<< HEAD
         armLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         armRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
@@ -87,5 +95,18 @@ public class Arm
         armLeft.setPower(lift);
         armRight.setPower(lift);
 
+=======
+        armLeft.setPower(0);
+        armRight.setPower(0);
+        armLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        armLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+>>>>>>> master
     }
+
+    public void moveByJoystick(float v){
+
+    };
 }
