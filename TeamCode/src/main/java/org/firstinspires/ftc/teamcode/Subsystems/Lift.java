46f234f178ec6f42145c34107ebb05abe394f188
David Atkinon <david.atkinson@netzero.com>
2020-06-18 22:21:02 -0500

    Updated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Lift.java
@@ -8,8 +8,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class Lift {
 
     // Define hardware objects
-    public DcMotor lift = null; // REV Core HEX motor
-    public DigitalChannel liftswitch = null;
+    public DcMotor liftmtr = null; // REV Core HEX motor
+    public DigitalChannel liftswitch;
 
 
     private static final int LIFT_STONE_LOAD = 50; // Left side reference
@@ -17,17 +17,17 @@ public class Lift {
     private static final int LIFT_ARM_STONE_LEVEL2 = 200;// Left side reference
     private static final double LIFT_SPEED = .5;
     private static final double LIFT_RESET_SPEED = -.2;
-    private static final int timeoutS = 2; // time out in seconds
+    private static final int timeoutS = 10; // time out in seconds
     private ElapsedTime runtime = new ElapsedTime();
 
     public void init(HardwareMap hwMap) {
 
         //motor
-        lift = hwMap.get(DcMotor.class, "Lift");
-        lift.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if needed
-        lift.setPower(0);
-        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftmtr = hwMap.get(DcMotor.class, "Lift");
+        liftmtr.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if needed
+        liftmtr.setPower(0);
+        liftmtr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        liftmtr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         // limit switch
         liftswitch = hwMap.get(DigitalChannel.class, "LiftSwitch");
         // set the digital channel to input.
@@ -36,36 +36,38 @@ public class Lift {
     }
 
     public void moveToPickup() {
-        lift.setTargetPosition(LIFT_STONE_LOAD);
-        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        lift.setPower(Math.abs(LIFT_SPEED));
+        liftmtr.setTargetPosition(LIFT_STONE_LOAD);
+        liftmtr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftmtr.setPower(Math.abs(LIFT_SPEED));
+
 
 
     }
 
     public void moveToStackLevel1() {
-        lift.setTargetPosition(LIFT_ARM_STONE_LEVEL1);
-        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        lift.setPower(Math.abs(LIFT_SPEED));
+        liftmtr.setTargetPosition(LIFT_ARM_STONE_LEVEL1);
+        liftmtr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftmtr.setPower(LIFT_SPEED);
 
     }
 
     public void moveToStackLevel2() {
-        lift.setTargetPosition(LIFT_ARM_STONE_LEVEL2);
-        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        lift.setPower(Math.abs(LIFT_SPEED));
+        liftmtr.setTargetPosition(LIFT_ARM_STONE_LEVEL2);
+        liftmtr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftmtr.setPower(Math.abs(LIFT_SPEED));
 
     }
 
     public void resetLift() {
         runtime.reset();
-        lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        lift.setPower(-LIFT_SPEED);
+        liftmtr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        liftmtr.setPower(-LIFT_SPEED);
         while ((runtime.seconds() < timeoutS) &&
                 (liftswitch.getState() == true )) {
+
             // put telemetry here when it gets sorted out
         }
-        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftmtr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        liftmtr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 }
