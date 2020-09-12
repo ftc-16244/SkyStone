59cd7d7905e9d28d1f073f6b41781dfaa91d55b7
David Atkinon <david.atkinson@netzero.com>
2020-09-12 00:19:10 -0500

    fixed lift telemetry calls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/Lift_Only_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/Lift_Only_Test.java
@@ -34,6 +34,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Enums.ArmState;
 import org.firstinspires.ftc.teamcode.Enums.DriveState;
 import org.firstinspires.ftc.teamcode.Subsystems.Arm;
@@ -115,7 +116,7 @@ public class Lift_Only_Test extends OpMode{
         }
 
         if (gamepad2.b) {
-            lift.resetLift();
+            lift.resetLift(telemetry);
             telemetry.addData("Reset", "Complete ");
         }
 
